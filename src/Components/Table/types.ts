c8af5ffb0b3baf2d7a3bd95c5006063505cb90e3
shreya-as <shreyathakuri1372@gmail.com>
2022-07-20 11:57:28 +0545

    resolved minor issues

--- src/Components/Table/types.ts
+++ src/Components/Table/types.ts
@@ -1,9 +1,12 @@
-import { appointmentProps } from "../../Pages/Appointment/types";
-import { doctorProps } from "../../Pages/Doctor/types";
-import { patientProps } from "../../Pages/Patient/types";
+import {
+  appointmentColumn,
+  appointmentProps,
+} from "../../Pages/Appointment/types";
+import { doctorColumn, doctorProps } from "../../Pages/Doctor/types";
+import { patientColumn, patientProps } from "../../Pages/Patient/types";
 export interface rowsValueProps {
   rowsValue: doctorProps[] | patientProps[] | appointmentProps[];
 }
 export interface TableProps extends rowsValueProps {
-  columns: any;
+  columns: appointmentColumn[] | patientColumn[] | doctorColumn[];
 }
